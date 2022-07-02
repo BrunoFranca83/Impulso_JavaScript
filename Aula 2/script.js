e7434fe994f5bd2eccf24747465f3f11981d2af3
“Brunofranca83” <Brunofranca83@yahoo.com.br>
2022-07-02 14:23:41 -0300

    First Commit

---
+++ Aula 2/script.js
@@ -0,0 +1,21 @@
+//tipos primitivos
+
+//Booleanos
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//Numero
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//String
+var nome = "Bruno";
+console.log(typeof(nome));
+
+//function
+var funcao = function(){}
+console.log(typeof(funcao));
+
+//como declarar
+var variavel = 'Bruno';
+variavel = 'França'
---
+++ readme.md
@@ -0,0 +1,9 @@
+2º Bootcamp - Impulso JavaScript
+
+Aqui é o objetivo é aprender JavaScript, com todas as dúvidas tiradas.
+
+Foco, Não desista jamais.
+
+Que Deus nos ajude!
+
+Bruno França
