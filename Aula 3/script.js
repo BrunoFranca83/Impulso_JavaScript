68dcce02a6d1c2978540bddd9cfa5021e04fbb04
“Brunofranca83” <Brunofranca83@yahoo.com.br>
2022-07-04 15:41:28 -0300

    Arrays e Objetos

--- Aula 3/script.js
+++ Aula 3/script.js
@@ -1,1 +1,45 @@
-//Vetores e 
+/*//Vetores e Objetos;
+
+//Como declarar um array
+
+let array = ['String', 1, true];
+console.log(array);
+
+//Pode guardar vários tipos de dados
+
+let arrayes = ['String', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(arrayes);*/
+
+//for each
+let array = ['String', 1, true];/*
+array.forEach(function(item, index) {console.log(item, index)});
+
+//Push
+array.push([]);
+console.log(array);
+
+//Pop()
+array.pop();
+console.log(array);
+
+//array.shift()
+array.shift();
+console.log(array);
+
+//array.unshift()
+array.unshift('Novo item de início');
+console.log(array);
+
+//IndexOf()
+console.log(array.indexOf(true));
+
+//splice()
+array.splice(0, 3);
+console.log(array);
+
+
+let novoArray = array.slice(0, 3);
+console.log(novoArray);*/
+
+let object = {string: 'string', number: 1, boolean: true, array: ["array"], objectInterno: 'objeto interno'};
+console.log(object);
